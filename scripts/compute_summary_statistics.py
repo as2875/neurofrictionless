97c01eb8076e4bb8d6a0e8a4bded5ea2f10d0d42
Alexander Shtyrov <as2875@cam.ac.uk>
2020-06-26 17:45:02 +0100

    Made script aware of spike trains

--- scripts/compute_summary_statistics.py
+++ scripts/compute_summary_statistics.py
@@ -8,33 +8,40 @@ import matplotlib.pyplot as plt
 DATA_DIR = r"C:\Users\sasha\Documents\fd_data"
 data_files = os.listdir(DATA_DIR)
 
-N_l = []
-mean_isi_l = []
-rate_l = []
-age_l = []
+mean_isi_pts = []
+rate_pts = []
+N_pts = []
 
 for file in data_files:
     # import package
     path = os.path.join(DATA_DIR, file)
     data = datapackage.Package(path)
     spikes = data.get_resource("spikes").read()
-    spike_times = [float(row[0]) for row in spikes]
+    spike_times = []
+    for row in spikes:
+        train_id = int(row[1])
+        spike_time = float(row[0])
+        try:
+            spike_times[train_id].append(spike_time)
+        except IndexError:
+            spike_times.append([spike_time])
     spike_times.sort()
     spike_trains = data.get_resource("spike-trains").read()
-    
-    # Compute summary statistics
-    N = len(spike_times)
-    rate = N/(spike_times[-1] - spike_times[0])
-    isi = numpy.diff(spike_times)
-    mean_isi = numpy.mean(isi)
-    
-    # Extract metadata
-    div = data.descriptor["age"]
-    age_l.append(div)
-    
-    # Print summary statistics
-    N_l.append(N)
-    rate_l.append(rate)
-    mean_isi_l.append(mean_isi)
 
-plt.plot(age_l, mean_isi_l, "k.", age_l, rate_l, "b.")
+    # Extract metadata
+    age = data.descriptor["age"]
+
+    # Compute summary statistics for each spike train
+    for train in spike_times:
+        N = len(train)
+        if N > 1:
+            rate = N/(train[-1] - train[0])
+        else:
+            continue
+        isi = numpy.diff(train)
+        mean_isi = numpy.mean(isi)
+        mean_isi_pts.append((age, mean_isi))
+        rate_pts.append((age, rate))
+        N_pts.append((age, N))
+
+plt.plot(mean_isi_pts, "k.", rate_pts, "b.")
