be5c2875e64000cc063056f007ead460dac56860
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-01 16:51:23 +0100

    Wrote class to convert MC Datatool to HDF5

--- h5fd/converter.py
+++ h5fd/converter.py
@@ -7,15 +7,21 @@ to a Frictionless Data Package.
 import csv
 import datapackage
 import h5py
+import itertools
 import os
 import pkg_resources
-import numpy
+import re
 
-class MEABurstConverter:
-    """A class for converting from HDF5 to Frictionless and back."""
+
+class BaseConverter:
+    """A base class for converters in this module."""
 
     def __init__(self):
-        self.formats = {}
+        self.formats = dict()
+
+
+class Hdf5FdConverter(BaseConverter):
+    """A class for converting from HDF5 to Frictionless and back."""
 
     def read(self, filename):
         """Read a file in either format."""
@@ -52,7 +58,8 @@ class MEABurstConverter:
                 # load data from HDF5 file
                 s_count = list(self.formats[".h5"]["sCount"])
                 spikes = list(self.formats[".h5"]["spikes"])
-                names = list(self.formats[".h5"]["names"])
+                if "names" in self.formats[".h5"].keys():
+                    names = list(self.formats[".h5"]["names"])
                 epos = list(self.formats[".h5"]["epos"])
 
                 # set up data package
@@ -90,11 +97,19 @@ class MEABurstConverter:
                     count = 0  # where in spikes we are
                     for i in range(len(s_count)):
                         # write to spike_trains.csv
-                        spike_trains_writer.writerow([i,
-                                                     names[i].decode(),
-                                                     epos[0][i],
-                                                     epos[1][i],
-                                                     s_count[i]])
+                        if "names" in self.formats[".h5"].keys():
+                            row = [i,
+                                   names[i].decode(),
+                                   epos[i][0],
+                                   epos[i][1],
+                                   s_count[i]]
+                        else:
+                            row = [i,
+                                   "",
+                                   epos[i][0],
+                                   epos[i][1],
+                                   s_count[i]]
+                        spike_trains_writer.writerow(row)
                         for j in range(count, count + s_count[i]):
                             # write to spikes.csv
                             spikes_writer.writerow([spikes[j],
@@ -107,8 +122,66 @@ class MEABurstConverter:
                 # remove csv files
                 os.remove(spike_trains_path)
                 os.remove(spikes_path)
+                self.formats[".h5"].close()
             elif ext == ".h5":
                 # convert from Frictionless to HDF5
                 raise NotImplementedError()
             else:
                 raise TypeError("Unsupported format")
+
+
+class McHdf5Converter(BaseConverter):
+    """A class for converting from MC Datatool files to HDF5."""
+
+    def read(self, filename):
+        ext = os.path.splitext(filename)[1]
+        if ext != ".txt":
+            raise TypeError("Unsupported format")
+        with open(filename) as f:
+            self.formats[ext] = f.read()
+
+    def write(self, filename):
+        # setup
+        index_pattern = re.compile(r"(?<=Spikes \d )\d\d")
+        whitespace_pattern = re.compile(r"\s+")
+
+        # extract spike times from file
+        channels = self.formats[".txt"].split("\n\n\n")
+        channels = channels[1:]  # remove heading
+
+        spikes = dict()
+        t_stop = 0
+        for channel in channels:
+            waveforms = [spike.strip().split("\n")
+                         for spike in channel.strip().split("\n\n")]
+            # exclude empty channels
+            if len(waveforms) <= 2:
+                continue
+
+            index = re.search(index_pattern, channel)
+            index = index.group(0)
+            spikes[index] = list()
+
+            for wave in waveforms:
+                spike_time = float(re.split(whitespace_pattern, wave[25])[0])
+                if spike_time > t_stop:
+                    t_stop = spike_time
+                spikes[index].append(spike_time)
+
+        assert spikes, "Recording is empty."
+
+        electrodes = list(spikes.keys())
+        trains = [spikes[e] for e in electrodes]
+
+        with h5py.File(filename, "w") as hdf:
+            spikes = list(itertools.chain(*trains))
+            s_count = [len(train) for train in trains]
+            epos = [(int(e[0]), int(e[1])) for e in electrodes]
+            hdf.create_dataset("spikes", data=spikes)
+            hdf.create_dataset("sCount", data=s_count)
+            hdf.create_dataset("epos", data=epos)
+            hdf.create_dataset("array", shape=())
+            hdf.create_group("meta")
+            hdf.create_group("summary")
+            hdf["summary"].create_dataset("N", data=len(spikes))
+            hdf["summary"].create_dataset("duration", data=t_stop)
