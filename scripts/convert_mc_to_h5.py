f6c087911879f6cb4506cb1c5305749467c780b4
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-01 18:28:19 +0100

    Wrote script to convert multiple raw MC Datatool files to HDF5

---
+++ scripts/convert_mc_to_h5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import h5fd.converter
+import os
+
+SOURCE_DIR = "../data/2020-02-21/"
+TARGET_DIR = "../data/2020-02-21_hdf5/"
+
+data_files = [file for file in os.listdir(SOURCE_DIR) if file.endswith(".txt")]
+m = h5fd.converter.McHdf5Converter()
+
+for file in data_files:
+    path = os.path.join(SOURCE_DIR, file)
+    m.read(path)
+    base = os.path.splitext(file)[0]
+    try:
+        m.write(os.path.join(TARGET_DIR, base + ".h5"))
+    except AssertionError:
+        continue
