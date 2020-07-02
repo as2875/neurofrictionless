9282105e52af0439549a4d8f12c64432d7d8648f
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-02 11:14:09 +0100

    Write to arbitrary target directory

--- scripts/convert_h5_to_fd.py
+++ scripts/convert_h5_to_fd.py
@@ -3,13 +3,14 @@
 import h5fd.converter
 import os
 
-DATA_DIR = ""
+SOURCE_DIR = "../data/2020-02-21_hdf5/"
+TARGET_DIR = "../data/2020-02-21_fd/"
 
-data_files = os.listdir(DATA_DIR)
-m = h5fd.converter.MEABurstConverter()
+data_files = os.listdir(SOURCE_DIR)
+m = h5fd.converter.Hdf5FdConverter()
 
 for file in data_files:
-    path = os.path.join(DATA_DIR, file)
+    path = os.path.join(SOURCE_DIR, file)
     m.read(path)
     base = os.path.splitext(file)[0]
-    m.write(base + ".zip")
+    m.write(os.path.join(TARGET_DIR, base + ".zip"))
