c939394e9fb3f37b0fe5ae4906dd84ee99352ac3
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-03 17:31:02 +0100

    Removed unnecessary if statements

--- h5fd/converter.py
+++ h5fd/converter.py
@@ -27,16 +27,15 @@ class Hdf5FdConverter(BaseConverter):
         """Read a file in either format."""
         # extract extension
         ext = os.path.splitext(filename)[1]
+        if ext != ".h5" or ext != ".zip":
+            raise TypeError("Unsupported format")
 
         if ext == ".h5":
             self.formats[".h5"] = h5py.File(filename, "r")
-            self.formats[".zip"] = None
-        elif ext == ".zip":
+        else:
             # not implemented yet
             # of doubtful utility
             raise NotImplementedError()
-        else:
-            raise TypeError("File in unsupported format")
 
     def write(self, filename):
         """Write a file in either format."""
@@ -44,90 +43,88 @@ class Hdf5FdConverter(BaseConverter):
         base = os.path.realpath(
             os.path.dirname(filename))
         ext = os.path.splitext(filename)[1]
+        assert ext == ".zip" or ext == ".h5", "Unsupported format"
 
         # if we haven't read anything, there is nothing to write
         assert self.formats, "Nothing to write"
 
-        if self.formats[ext]:
-            # if we already have the correct format, write immediately
-            # not implemented yet
+        if ext == ".zip":
+            # convert from HDF5 to Frictionless
+            # load data from HDF5 file
+            assert ".h5" in self.formats.keys(), "No HDF5 file to convert from"
+
+            s_count = list(self.formats[".h5"]["sCount"])
+            spikes = list(self.formats[".h5"]["spikes"])
+            if "names" in self.formats[".h5"].keys():
+                names = list(self.formats[".h5"]["names"])
+            epos = list(self.formats[".h5"]["epos"])
+
+            # set up data package
+            datapackage_path = pkg_resources.resource_filename(
+                __package__,
+                "datapackage.json")
+            package = datapackage.Package(
+                base_path=base,
+                descriptor=datapackage_path)
+
+            # add metadata to data package descriptor
+            for k, v in self.formats[".h5"]["meta"].items():
+                value = v[0].item()
+                if isinstance(value, bytes):
+                    value = value.decode()
+                package.descriptor[k] = value
+
+            package.commit()
+
+            # extract resource names
+            spike_trains_path = package.get_resource("spike-trains").source
+            spikes_path = package.get_resource("spikes").source
+
+            with open(spike_trains_path, "w", newline="") as spike_trains_file, open(spikes_path, "w", newline="") as spikes_file:
+                # initialise writers
+                spike_trains_writer = csv.writer(spike_trains_file)
+                spikes_writer = csv.writer(spikes_file)
+
+                # write headers
+                spike_trains_fields = [field.name for field in package.get_resource("spike-trains").schema.fields]
+                spike_trains_writer.writerow(spike_trains_fields)
+                spikes_fields = [field.name for field in package.get_resource("spikes").schema.fields]
+                spikes_writer.writerow(spikes_fields)
+
+                count = 0  # where in spikes we are
+                for i in range(len(s_count)):
+                    # write to spike_trains.csv
+                    if "names" in self.formats[".h5"].keys():
+                        row = [i,
+                               names[i].decode(),
+                               epos[i][0],
+                               epos[i][1],
+                               s_count[i]]
+                    else:
+                        row = [i,
+                               "",
+                               epos[i][0],
+                               epos[i][1],
+                               s_count[i]]
+                    spike_trains_writer.writerow(row)
+                    for j in range(count, count + s_count[i]):
+                        # write to spikes.csv
+                        spikes_writer.writerow([spikes[j],
+                                                i])
+                    count += s_count[i]
+
+            # zip, zip
+            package.save(filename)
+
+            # remove csv files
+            os.remove(spike_trains_path)
+            os.remove(spikes_path)
+            self.formats[".h5"].close()
+        elif ext == ".h5":
+            # convert from Frictionless to HDF5
             raise NotImplementedError()
         else:
-            if ext == ".zip":
-                # convert from HDF5 to Frictionless
-                # load data from HDF5 file
-                s_count = list(self.formats[".h5"]["sCount"])
-                spikes = list(self.formats[".h5"]["spikes"])
-                if "names" in self.formats[".h5"].keys():
-                    names = list(self.formats[".h5"]["names"])
-                epos = list(self.formats[".h5"]["epos"])
-
-                # set up data package
-                datapackage_path = pkg_resources.resource_filename(
-                    __package__,
-                    "datapackage.json")
-                package = datapackage.Package(
-                    base_path=base,
-                    descriptor=datapackage_path)
-
-                # add metadata to data package descriptor
-                for k, v in self.formats[".h5"]["meta"].items():
-                    value = v[0].item()
-                    if isinstance(value, bytes):
-                        value = value.decode()
-                    package.descriptor[k] = value
-
-                package.commit()
-
-                # extract resource names
-                spike_trains_path = package.get_resource("spike-trains").source
-                spikes_path = package.get_resource("spikes").source
-
-                with open(spike_trains_path, "w", newline="") as spike_trains_file, open(spikes_path, "w", newline="") as spikes_file:
-                    # initialise writers
-                    spike_trains_writer = csv.writer(spike_trains_file)
-                    spikes_writer = csv.writer(spikes_file)
-
-                    # write headers
-                    spike_trains_fields = [field.name for field in package.get_resource("spike-trains").schema.fields]
-                    spike_trains_writer.writerow(spike_trains_fields)
-                    spikes_fields = [field.name for field in package.get_resource("spikes").schema.fields]
-                    spikes_writer.writerow(spikes_fields)
-
-                    count = 0  # where in spikes we are
-                    for i in range(len(s_count)):
-                        # write to spike_trains.csv
-                        if "names" in self.formats[".h5"].keys():
-                            row = [i,
-                                   names[i].decode(),
-                                   epos[i][0],
-                                   epos[i][1],
-                                   s_count[i]]
-                        else:
-                            row = [i,
-                                   "",
-                                   epos[i][0],
-                                   epos[i][1],
-                                   s_count[i]]
-                        spike_trains_writer.writerow(row)
-                        for j in range(count, count + s_count[i]):
-                            # write to spikes.csv
-                            spikes_writer.writerow([spikes[j],
-                                                    i])
-                        count += s_count[i]
-
-                # zip, zip
-                package.save(filename)
-
-                # remove csv files
-                os.remove(spike_trains_path)
-                os.remove(spikes_path)
-                self.formats[".h5"].close()
-            elif ext == ".h5":
-                # convert from Frictionless to HDF5
-                raise NotImplementedError()
-            else:
-                raise TypeError("Unsupported format")
+            raise TypeError("Unsupported format")
 
 
 class McHdf5Converter(BaseConverter):
@@ -143,7 +140,7 @@ class McHdf5Converter(BaseConverter):
             self.formats[ext] = self.formats[ext][30:]  # chew off header
 
     def write(self, filename):
-        assert filename.endswith(".h5"), "Filename must end with '.h5'."
+        assert filename.endswith(".h5"), "Filename must end with '.h5'"
 
         # setup
         index_pattern = re.compile(r"(?<=Spikes \d )\d\d")
