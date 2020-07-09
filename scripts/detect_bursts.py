0f1dc67bfbb8fc4cabba16ec86931d9e7642acaf
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-09 11:50:11 +0100

    Use module function to extract data

--- scripts/detect_bursts.py
+++ scripts/detect_bursts.py
@@ -3,6 +3,7 @@
 import datapackage
 import os
 import h5fd.plot
+import quantities as qt
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -31,16 +32,9 @@ mi_par = ListVector({"beg_isi": 0.17,
 with PdfPages(FIGURE_PATH) as pdf:
     for file in data_files:
         package = datapackage.Package(file)
-        channels, t_stop = {}, 0
-        for row in package.get_resource("spikes").read(keyed=True, relations=True):
-            spike_time = float(row["time"]) / 1000
-            index = row["spike-train-index"]["epos-x"] + \
-                row["spike-train-index"]["epos-y"]
-            if index not in channels.keys():
-                channels[index] = []
-            channels[index].append(spike_time)
-            if spike_time > t_stop:
-                t_stop = spike_time
+        _, channels, t_stop = h5fd.plot.extract_spike_trains(package,
+                                                             qt.ms,
+                                                             qt.s)
 
         # raster plot
         figure, axes = plt.subplots()
@@ -61,6 +55,6 @@ with PdfPages(FIGURE_PATH) as pdf:
                 end_b = int(burst[1]) - 1
                 axes.plot((spikes[start_b], spikes[end_b]),
                           (y_offsets_map[channel], y_offsets_map[channel]))
-        figure.tight_layout()
+
         pdf.savefig()
         plt.close()
