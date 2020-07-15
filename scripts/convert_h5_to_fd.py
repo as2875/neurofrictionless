dc994bc715521938f5e7c5ae54377bc02b7f379a
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-15 14:47:17 +0100

    Change paths in conversion script

--- scripts/convert_h5_to_fd.py
+++ scripts/convert_h5_to_fd.py
@@ -2,14 +2,15 @@
 
 import h5fd.converter
 import os
+from tqdm import tqdm
 
-SOURCE_DIR = "../data/2020-02-21_hdf5/"
-TARGET_DIR = "../data/2020-02-21_fd/"
+SOURCE_DIR = "../data/g2chvcdata/"
+TARGET_DIR = "../data/g2chvcdata_fd/"
 
-data_files = os.listdir(SOURCE_DIR)
+data_files = [file for file in os.listdir(SOURCE_DIR) if file.endswith(".h5")]
 m = h5fd.converter.Hdf5FdConverter()
 
-for file in data_files:
+for file in tqdm(data_files):
     path = os.path.join(SOURCE_DIR, file)
     m.read(path)
     base = os.path.splitext(file)[0]
