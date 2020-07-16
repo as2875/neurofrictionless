1b07f476306189963c82d3a0e526ed5a8ae0ae8f
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-16 14:28:25 +0100

    Wrote script to compute mean correlation

--- scripts/README.md
+++ scripts/README.md
@@ -7,6 +7,7 @@
 | `detect_bursts.py` | Detects bursts. Produces raster plots with bursts represented by coloured lines. |
 | `compute_isi_distribution.py` | Computes interspike intervals (ISIs) for each channel. Plots histograms of ISIs. |
 | `compute_summary_statistics.py` | Computes simple summary statistics. Plots as a series of scatter graphs. |
-| `convert_h5_to_fd` | Simple script to convert a directory of HDF5 files to a Frictionless format using `h5fd.converter`. |
-| `convert_mc_to_h5` | Simple script to convert a directory of MC Datatool files to HDF5 using `h5fd.converter`. |
+| `convert_h5_to_fd.py` | Simple script to convert a directory of HDF5 files to a Frictionless format using `h5fd.converter`. |
+| `convert_mc_to_h5.py` | Simple script to convert a directory of MC Datatool files to HDF5 using `h5fd.converter`. |
+| `correlate_pairwise.py` | Plot mean correlation against age. |
 | `process_mcdatatool.py` | Produces summary plots from the raw data. Useful for validating the data conversion process. |
---
+++ scripts/correlate_pairwise.py
@@ -0,0 +1,82 @@
+import datapackage
+import datetime
+import elephant
+import h5fd.plot
+import numpy
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import quantities as qt
+from h5fd.plot import RECORDING_ATTEMPTS
+from scipy.stats import sem
+
+DATA_DIR = "../data/2020-02-21_fd/"
+data_files = [os.path.join(DATA_DIR, file) for file in os.listdir(DATA_DIR)]
+
+# plotting parameters
+FIGURE_PATH = "../plots/correlation_plots.pdf"
+matplotlib.rcParams["figure.dpi"] = 300
+matplotlib.rcParams["figure.figsize"] = [5, 10]
+
+BINW = 0.5 * qt.s
+
+age_l, corr_l = [], []
+err_l = []
+colours = {"by-replicate": [], "by-recording": []}
+for file in data_files:
+    # extract data
+    package = datapackage.Package(file)
+    _, channels, _ = h5fd.plot.extract_spike_trains(package, qt.ms, qt.s)
+    trains = list(channels.values())
+    if not trains:
+        continue
+    # bin
+    b_trains = elephant.conversion.BinnedSpikeTrain(trains, binsize=BINW)
+    corr = elephant.spike_train_correlation.corrcoef(b_trains)
+    # if the matrix is not a matrix
+    if not corr.shape:
+        continue
+    # matrix is symmetric, take upper triangle
+    corr_tril = numpy.triu(corr, k=1)
+    n = (corr.shape[0] * (corr.shape[1] - 1)) / 2
+    mean_corr = numpy.sum(corr_tril) / n
+    corr_l.append(mean_corr)
+    # standard error in the mean
+    nonz = corr_tril.ravel()[numpy.flatnonzero(corr)]
+    err = sem(nonz)
+    err_l.append(err)
+    # extract age
+    age = int(package.descriptor["meta"]["age"])
+    age_l.append(age)
+    # which colour?
+    if package.descriptor["meta"]["MEA"] == "2539":
+        colours["by-replicate"].append("r")
+    else:
+        colours["by-replicate"].append("b")
+
+    datestamp = package.descriptor["meta"]["date"]
+    recording_date = datetime.date(int("20" + datestamp[:2]),
+                                   int(datestamp[2:4]),
+                                   int(datestamp[4:]))
+    if RECORDING_ATTEMPTS[0][0] <= recording_date <= RECORDING_ATTEMPTS[0][1]:
+        colours["by-recording"].append("r")
+    elif RECORDING_ATTEMPTS[1][0] <= recording_date <= RECORDING_ATTEMPTS[1][1]:
+        colours["by-recording"].append("b")
+    else:
+        colours["by-recording"].append("k")
+
+figure, axes = plt.subplots(2, 1)
+
+axes[0].set_title("Plot by replicate")
+axes[0].set_xlabel("age / DIV")
+axes[0].set_ylabel("correlation")
+axes[0].scatter(age_l, corr_l, c=colours["by-replicate"])
+axes[0].errorbar(age_l, corr_l, fmt="none", yerr=err_l, ecolor="k")
+
+axes[1].set_title("Plot by recording")
+axes[1].set_xlabel("age / DIV")
+axes[1].set_ylabel("correlation")
+axes[1].scatter(age_l, corr_l, c=colours["by-recording"])
+axes[1].errorbar(age_l, corr_l, fmt="none", yerr=err_l, ecolor="k")
+
+plt.savefig(FIGURE_PATH)
