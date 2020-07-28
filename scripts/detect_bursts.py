1976d076e209369471b92b5d477adfef34cabd48
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-28 16:47:33 +0100

    Generate 4 rasters as PNGs for use in manuscript

--- scripts/detect_bursts.py
+++ scripts/detect_bursts.py
@@ -14,13 +14,20 @@ from rpy2.robjects.vectors import ListVector, FloatVector
 meaRtools = importr("meaRtools")
 
 # plot parameters
+matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 matplotlib.rcParams["figure.dpi"] = 300
 matplotlib.rcParams["font.size"] = 6.0
 FIGURE_PATH = "../plots/burst_plots.pdf"
+REPRESENTATIVE_PLOT_BASE_PATH = "../plots/burst_plot_"
 
 # location of data
 DATA_DIR = "../data/2020-02-21_fd/"
-data_files = [os.path.join(DATA_DIR, file) for file in os.listdir(DATA_DIR)]
+data_files = sorted([os.path.join(DATA_DIR, file)
+                     for file in os.listdir(DATA_DIR)])
+REPRESENTATIVE_PLOTS = ["../data/2020-02-21_fd/170922_D15_2540.zip",
+                        "../data/2020-02-21_fd/170927_D20_2540.zip",
+                        "../data/2020-02-21_fd/180413_D27_2540.zip",
+                        "../data/2020-02-21_fd/180504_D48_2539.zip"]
 
 # parameters for burst detection
 mi_par = ListVector({"beg_isi": 0.17,
@@ -29,16 +36,22 @@ mi_par = ListVector({"beg_isi": 0.17,
                      "min_durn": 0.05,
                      "min_spikes": 3})
 
+# generate multipage PDF
+count = 1
 with PdfPages(FIGURE_PATH) as pdf:
     for file in data_files:
         package = datapackage.Package(file)
         _, channels, t_stop = h5fd.plot.extract_spike_trains(package,
                                                              qt.ms,
                                                              qt.s)
+        date = package.descriptor["meta"]["date"]
+        age = str(package.descriptor["meta"]["age"])
+        mea = package.descriptor["meta"]["MEA"]
+        title = date + " " + "D" + age + " " + "R" + mea
 
         # raster plot
         figure, axes = plt.subplots()
-        y_offsets_map = h5fd.plot.rasterplot(channels, axes, file, "s")
+        y_offsets_map = h5fd.plot.rasterplot(channels, axes, title, "s")
 
         if y_offsets_map is None:
             pdf.savefig()
@@ -57,4 +70,7 @@ with PdfPages(FIGURE_PATH) as pdf:
                           (y_offsets_map[channel], y_offsets_map[channel]))
 
         pdf.savefig()
+        if file in REPRESENTATIVE_PLOTS:
+            figure.savefig(REPRESENTATIVE_PLOT_BASE_PATH + str(count) + ".png")
+            count += 1
         plt.close()
