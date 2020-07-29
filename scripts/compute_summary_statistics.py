a12914766f73dad03205d48a44889e947d98d10d
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-29 18:11:15 +0100

    Different shapes to denote different replicates in summary plots

--- scripts/compute_summary_statistics.py
+++ scripts/compute_summary_statistics.py
@@ -13,22 +13,23 @@ import quantities as qt
 from matplotlib.backends.backend_pdf import PdfPages
 
 # adjust matplotlib parameters
+matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 matplotlib.rcParams["figure.dpi"] = 300
-matplotlib.rcParams["figure.figsize"] = [10, 10]
+matplotlib.rcParams["figure.figsize"] = [10, 6]
 matplotlib.rcParams["figure.constrained_layout.use"] = True
-matplotlib.rcParams["font.size"] = 10.0
+matplotlib.rcParams["font.size"] = 12.0
 
 DATA_DIR = "../data/2020-02-21_fd/"
 FIGURE_FILE = "../plots/development_plots.png"
 data_files = [os.path.join(DATA_DIR, file) for file in os.listdir(DATA_DIR)]
 
-age_l, fr_l, N_l, active_channels_l, ts_l, fr_perchan_l = \
-    [], [], [], [], [], []
-fr_errors = [[], []]
-colours = {"by-replicate": [], "by-recording": []}
+age_l, fr_l, N_l, active_channels_l, ts_l, fr_perchan_l, colours =\
+    ({"2539": [], "2540": []} for i in range(7))
+fr_errors = {"2539": [[], []], "2540": [[], []]}
 for file in data_files:
     # load package
     package = datapackage.Package(file)
+    mea = package.descriptor["meta"]["MEA"]
 
     # spike times
     spikes, channels, t_stop = h5fd.plot.extract_spike_trains(package,
@@ -36,14 +37,14 @@ for file in data_files:
                                                               qt.s)
 
     age = package.descriptor["meta"]["age"]
-    age_l.append(age)
+    age_l[mea].append(age)
 
     # compute mean firing rate
     if spikes.any():
         firing_rate = elephant.statistics.mean_firing_rate(spikes)
     else:
         firing_rate = None
-    fr_l.append(firing_rate)
+    fr_l[mea].append(firing_rate)
 
     # firing rate per channel
     if channels:
@@ -51,100 +52,75 @@ for file in data_files:
             elephant.statistics.mean_firing_rate(train))
                       for train in channels.values()]
         mean_fr = statistics.mean(channel_fr)
-        fr_perchan_l.append(mean_fr)
+        fr_perchan_l[mea].append(mean_fr)
         # upper errors
-        fr_errors[1].append(
+        fr_errors[mea][1].append(
             max(channel_fr) - mean_fr
             )
         # lower errors
-        fr_errors[0].append(
+        fr_errors[mea][0].append(
             mean_fr - min(channel_fr)
             )
     else:
-        fr_perchan_l.append(0)
-        fr_errors[0].append(0)
-        fr_errors[1].append(0)
+        fr_perchan_l[mea].append(0)
+        fr_errors[mea][0].append(0)
+        fr_errors[mea][1].append(0)
 
     # number of spikes
-    N_l.append(len(spikes))
+    N_l[mea].append(len(spikes))
 
     # number of active channels
     spike_trains = package.get_resource("spike-trains").read()
-    active_channels_l.append(len(spike_trains))
+    active_channels_l[mea].append(len(spike_trains))
 
     # computed recording time
-    ts_l.append(t_stop)
-
-    # which colour to use?
-    if package.descriptor["meta"]["MEA"] == "2539":
-        colours["by-replicate"].append("r")
-    elif package.descriptor["meta"]["MEA"] == "2540":
-        colours["by-replicate"].append("b")
-    else:
-        raise BaseException("Something is wrong.")
+    ts_l[mea].append(t_stop)
 
     datestamp = package.descriptor["meta"]["date"]
     recording_date = datetime.date(int("20" + datestamp[:2]),
                                    int(datestamp[2:4]),
                                    int(datestamp[4:]))
     if RECORDING_ATTEMPTS[0][0] <= recording_date <= RECORDING_ATTEMPTS[0][1]:
-        colours["by-recording"].append("r")
+        colours[mea].append("r")
     elif RECORDING_ATTEMPTS[1][0] <= recording_date <= RECORDING_ATTEMPTS[1][1]:
-        colours["by-recording"].append("b")
+        colours[mea].append("b")
     elif RECORDING_ATTEMPTS[2][0] <= recording_date <= RECORDING_ATTEMPTS[2][1]:
-        colours["by-recording"].append("k")
+        colours[mea].append("k")
     else:
         raise BaseException("Something is wrong.")
 
 # plot data
 # by replicate
-figure, axes = plt.subplots(4, 3, sharex=True)
+figure, axes = plt.subplots(2, 3, sharex=True)
 
-axes[0, 0].scatter(age_l, fr_l, c=colours["by-replicate"])
+axes[0, 0].scatter(age_l["2539"], fr_l["2539"], c=colours["2539"], marker="s")
+axes[0, 0].scatter(age_l["2540"], fr_l["2540"], c=colours["2540"], marker="o")
 axes[0, 0].set_xlabel("age / DIV")
 axes[0, 0].set_ylabel("firing rate / $s^{-1}$")
 
-axes[0, 1].scatter(age_l, N_l, c=colours["by-replicate"])
+axes[0, 1].scatter(age_l["2539"], N_l["2539"], c=colours["2539"], marker="s")
+axes[0, 1].scatter(age_l["2540"], N_l["2540"], c=colours["2540"], marker="o")
 axes[0, 1].set_xlabel("age / DIV")
 axes[0, 1].set_ylabel("number of spikes")
 
-axes[0, 2].scatter(age_l, active_channels_l, c=colours["by-replicate"])
+axes[0, 2].scatter(age_l["2539"], active_channels_l["2539"], c=colours["2539"], marker="s")
+axes[0, 2].scatter(age_l["2540"], active_channels_l["2540"], c=colours["2540"], marker="o")
 axes[0, 2].set_xlabel("age / DIV")
 axes[0, 2].set_ylabel("active channels")
 
-axes[1, 0].scatter(age_l, ts_l, c=colours["by-replicate"])
+axes[1, 0].scatter(age_l["2539"], ts_l["2539"], c=colours["2539"], marker="s")
+axes[1, 0].scatter(age_l["2540"], ts_l["2540"], c=colours["2540"], marker="o")
 axes[1, 0].set_xlabel("age / DIV")
 axes[1, 0].set_ylabel("recording time / $s$")
 
-axes[1, 1].scatter(age_l, fr_perchan_l, c=colours["by-replicate"])
-axes[1, 1].errorbar(age_l, fr_perchan_l, fmt="none", yerr=fr_errors)
+axes[1, 1].scatter(age_l["2539"], fr_perchan_l["2539"], c=colours["2539"], marker="s")
+axes[1, 1].scatter(age_l["2540"], fr_perchan_l["2540"], c=colours["2540"], marker="o")
+axes[1, 1].errorbar(age_l["2539"], fr_perchan_l["2539"], fmt="none", yerr=fr_errors["2539"], ecolor="k")
+axes[1, 1].errorbar(age_l["2540"], fr_perchan_l["2540"], fmt="none", yerr=fr_errors["2540"], ecolor="k")
 axes[1, 1].set_xlabel("age / DIV")
 axes[1, 1].set_ylabel("firing rate\nper channel / $s^{-1}$")
 
 axes[1, 2].set_axis_off()
-
-axes[2, 0].scatter(age_l, fr_l, c=colours["by-recording"])
-axes[2, 0].set_xlabel("age / DIV")
-axes[2, 0].set_ylabel("firing rate / $s^{-1}$")
-
-axes[2, 1].scatter(age_l, N_l, c=colours["by-recording"])
-axes[2, 1].set_xlabel("age / DIV")
-axes[2, 1].set_ylabel("number of spikes")
-
-axes[2, 2].scatter(age_l, active_channels_l, c=colours["by-recording"])
-axes[2, 2].set_xlabel("age / DIV")
-axes[2, 2].set_ylabel("active channels")
-
-axes[3, 0].scatter(age_l, ts_l, c=colours["by-recording"])
-axes[3, 0].set_xlabel("age / DIV")
-axes[3, 0].set_ylabel("recording time / $s$")
-
-axes[3, 1].scatter(age_l, fr_perchan_l, c=colours["by-recording"])
-axes[3, 1].errorbar(age_l, fr_perchan_l, fmt="none", yerr=fr_errors)
-axes[3, 1].set_xlabel("age / DIV")
-axes[3, 1].set_ylabel("firing rate\nper channel / $s^{-1}$")
-
-axes[3, 2].set_axis_off()
 
 # save figure
 plt.savefig(FIGURE_FILE)
