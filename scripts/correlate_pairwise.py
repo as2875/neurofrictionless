f23caf5d2437a3deea84c2e28839649deea74302
Alexander Shtyrov <as2875@cam.ac.uk>
2020-07-29 18:31:04 +0100

    Different shapes to denote different replicates in correlation plot

--- scripts/correlate_pairwise.py
+++ scripts/correlate_pairwise.py
@@ -17,16 +17,15 @@ data_files = [os.path.join(DATA_DIR, file) for file in os.listdir(DATA_DIR)]
 FIGURE_PATH = "../plots/correlation_plots.png"
 matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 matplotlib.rcParams["figure.dpi"] = 300
-matplotlib.rcParams["figure.figsize"] = [10, 5]
+matplotlib.rcParams["figure.figsize"] = [5, 5]
 
 BINW = 0.5 * qt.s
 
-age_l, corr_l = [], []
-err_l = []
-colours = {"by-replicate": [], "by-recording": []}
+age_l, corr_l, colours, err_l = ({"2539": [], "2540": []} for i in range(4))
 for file in data_files:
     # extract data
     package = datapackage.Package(file)
+    mea = package.descriptor["meta"]["MEA"]
     _, channels, _ = h5fd.plot.extract_spike_trains(package, qt.ms, qt.s)
     trains = list(channels.values())
     if len(trains) < 2:
@@ -48,44 +47,32 @@ for file in data_files:
     corr_triu = numpy.triu(corr, k=1)
     n = (corr.shape[0] * (corr.shape[1] - 1)) / 2
     mean_corr = numpy.sum(corr_triu) / n
-    corr_l.append(mean_corr)
+    corr_l[mea].append(mean_corr)
     # standard error in the mean
     nonz = corr_triu.ravel()[numpy.flatnonzero(corr_triu)]
     err = numpy.std(nonz)
-    err_l.append(err)
+    err_l[mea].append(err)
     # extract age
     age = int(package.descriptor["meta"]["age"])
-    age_l.append(age)
-
-    # which colour?
-    if package.descriptor["meta"]["MEA"] == "2539":
-        colours["by-replicate"].append("r")
-    else:
-        colours["by-replicate"].append("b")
+    age_l[mea].append(age)
 
     datestamp = package.descriptor["meta"]["date"]
     recording_date = datetime.date(int("20" + datestamp[:2]),
                                    int(datestamp[2:4]),
                                    int(datestamp[4:]))
     if RECORDING_ATTEMPTS[0][0] <= recording_date <= RECORDING_ATTEMPTS[0][1]:
-        colours["by-recording"].append("r")
+        colours[mea].append("r")
     elif RECORDING_ATTEMPTS[1][0] <= recording_date <= RECORDING_ATTEMPTS[1][1]:
-        colours["by-recording"].append("b")
+        colours[mea].append("b")
     else:
-        colours["by-recording"].append("k")
+        colours[mea].append("k")
 
-figure, axes = plt.subplots(1, 2, sharey=True)
+figure, axes = plt.subplots()
 
-axes[0].set_title("A")
-axes[0].set_xlabel("age / DIV")
-axes[0].set_ylabel("STTC")
-axes[0].scatter(age_l, corr_l, c=colours["by-replicate"])
-axes[0].errorbar(age_l, corr_l, fmt="none", yerr=err_l, ecolor="k")
-
-axes[1].set_title("B")
-axes[1].set_xlabel("age / DIV")
-axes[1].set_ylabel("STTC")
-axes[1].scatter(age_l, corr_l, c=colours["by-recording"])
-axes[1].errorbar(age_l, corr_l, fmt="none", yerr=err_l, ecolor="k")
-
+axes.set_xlabel("age / DIV")
+axes.set_ylabel("STTC")
+axes.scatter(age_l["2539"], corr_l["2539"], c=colours["2539"], marker="s")
+axes.scatter(age_l["2540"], corr_l["2540"], c=colours["2540"], marker="o")
+axes.errorbar(age_l["2539"], corr_l["2539"], fmt="none", yerr=err_l["2539"], ecolor="k")
+axes.errorbar(age_l["2540"], corr_l["2540"], fmt="none", yerr=err_l["2540"], ecolor="k")
 plt.savefig(FIGURE_PATH)
