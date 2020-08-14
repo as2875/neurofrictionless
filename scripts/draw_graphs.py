02436826a91c811edf1f4a3e33c2e8c6fe13dfcd
Alexander Shtyrov <as2875@cam.ac.uk>
2020-08-14 10:33:34 +0100

    Make transparency of edges depend on STTC

--- scripts/draw_graphs.py
+++ scripts/draw_graphs.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
+from tqdm import tqdm
 
 INPUT_PACKAGE = "../plots/points/correlation_plots.zip"
 FIGURE_PATH = "../plots/correlation_graphs.pdf"
@@ -11,8 +12,10 @@ package = datapackage.Package(INPUT_PACKAGE)
 nodes = [str(i) + str(j) for i in range(1, 9) for j in range(1, 9)]
 positions = {node: (int(node[0]) - 1, int(node[1]) - 1) for node in nodes}
 
+resources = sorted(package.resources, key=lambda x: x.descriptor["date"])
+resources = sorted(resources, key=lambda x: x.descriptor["replicate"])
 with PdfPages(FIGURE_PATH) as pdf:
-    for resource in package.resources:
+    for resource in tqdm(resources):
         G = nx.Graph()
         G.add_nodes_from(nodes)
         matrix = resource.read()
@@ -21,7 +24,14 @@ with PdfPages(FIGURE_PATH) as pdf:
             for j in range(i, len(matrix[i])):
                 G.add_edge(channels[i], channels[j], weight=matrix[i][j])
         edges = G.edges()
-        weights = [int(G[i][j]["weight"] > 0.7) for i, j in edges]
-        nx.draw(G, pos=positions, edges=edges, width=weights)
-        plt.tight_layout()
+        weights = [abs(G[i][j]["weight"]) for i, j in edges]
+        figure, axes = plt.subplots()
+        nx.draw_networkx_nodes(G, pos=positions, node_size=100, ax=axes)
+        for e, w in zip(edges, weights):
+            nx.draw_networkx_edges(G, pos=positions, edgelist=[e], alpha=w, ax=axes)
+        figure.suptitle(resource.descriptor["date"] + " D" +
+                        str(resource.descriptor["age"]) + " " +
+                        resource.descriptor["replicate"])
+        figure.tight_layout()
         pdf.savefig()
+        plt.close()
